08ad54fe355a78fd1adcd955a693e1c904f0c8fd
DariuszKny <nasz_email_do_github>
2022-05-09 21:00:33 +0200

    WPA-40 encja- User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, Min, Max,Matches,Validate } from "class-validator";
+import { CustomMatchPasswords } from "src/users/CustomMatchPasswords ";
+
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+    @IsNotEmpty()
+    @Validate(CustomMatchPasswords, ['repassword'])
+    password: string;
+    @IsNotEmpty()
+    repassword: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  @InjectRepository(User)
+  private readonly repository: Repository<User>;
+
+  create(body: CreateUserDto) {
+
+    let user = new User()
+    user.email = body.email
+    user.password = body.password
+
+    this.repository.save(user);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.repository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.repository.createQueryBuilder("user")
+    .where("user.id = " + id)
+    .getOne()
+    
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, body: UpdateUserDto) {
+
+   return this.repository.findOne({
+     id:id
+   }).then(user => {
+      user.email = body.email
+      user.password = body.password
+      this.repository.save(user);
+      return user
+    })
+
+  
+
   }
 
   remove(id: number) {
